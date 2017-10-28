fbea34c5b9f13b7258ce88879e8ba4aee48fc340
leonzyz <leonzyz@gmail.com>
2017-10-28 14:02:36 +0800

    update readcfg script

--- cfg/readcfg.m
+++ cfg/readcfg.m
@@ -9,3 +9,11 @@
 
 Cfg.MicNum=4;	%microphone number
 Cfg.MicDist=4.1e-2;	%microphone distance
+Cfg.SourceType=0;	%0=single tone,1=voice
+Cfg.SourceFreq=1e3;	%only for SourceType=0
+Cfg.SourceLen=1e3;	%only for SourceType=0
+Cfg.SourceFilename="test.adc";	%only for SourceType=1
+Cfg.NoiseTpye=0;	%Interference typ,0=awgn,1=noise source
+Cfg.NoiseFilename="noise.wav";	%only for SourceType=1
+Cfg.SNR=10;	%SNR of single microphone
+Cfg.SIR=10;	%SIR of single microphone
